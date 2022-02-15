c11717f42651a2970d284b064f65a5f3da135664
Chen Harel <chook.harel@gmail.com>
2022-02-15 15:41:11 -0600

    fixes

--- src/frontend/middleware.go
+++ src/frontend/middleware.go
@@ -22,9 +22,9 @@ import (
 	"github.com/google/uuid"
 	"github.com/sirupsen/logrus"
 	//"github.com/uptrace/opentelemetry-go-extra/otellogrus"
-	// "go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/trace"
-	// "go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/attribute"
 )
 
 type ctxKeyLog struct{}
@@ -75,10 +75,10 @@ func (lh *logHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	var span trace.Span = trace.SpanFromContext(r.Context())
 
-	// _, span := otel.Tracer("manual").Start(ctx, "Middleware")
+	_, span := otel.Tracer("manual").Start(ctx, "Middleware")
 	if span != nil {
-		// span.SetAttributes(attribute.String("method", r.Method))
-		// span.SetAttributes(attribute.String("path", r.URL.Path))
+		span.SetAttributes(attribute.String("method", r.Method))
+		span.SetAttributes(attribute.String("path", r.URL.Path))
 
 		var spanContext trace.SpanContext = span.SpanContext()
 		
@@ -101,8 +101,8 @@ func (lh *logHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			log.Errorf("request %s %s failed with status: %d. requestID: %s", r.Method, r.URL.Path, rr.status, requestID.String())
 		}
 
-		// span.SetAttributes(attribute.Int("status_code", rr.status))
-		// span.End()
+		span.SetAttributes(attribute.Int("status_code", rr.status))
+		span.End()
 	}()
 
 	ctx = context.WithValue(ctx, ctxKeyLog{}, log)
