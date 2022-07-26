6fd35ba63458d7322618c059609a4d845c57b666
Chen Harel <chook.harel@gmail.com>
2022-07-25 23:11:21 -0500

    middleware random more ips

--- src/frontend/middleware.go
+++ src/frontend/middleware.go
@@ -16,10 +16,11 @@ package main
 
 import (
 	"context"
-	"go.opentelemetry.io/otel/trace"
 	"math/rand"
 	"net/http"
 	"time"
+
+	"go.opentelemetry.io/otel/trace"
 
 	"github.com/google/uuid"
 	"github.com/sirupsen/logrus"
@@ -72,8 +73,8 @@ func (lh *logHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		"http_req_method": r.Method,
 		"http_req_id":     requestID.String(),
 		"http_req_ip":     readUserIP(r),
-		"trace_id": 	   spanContext.TraceID(),
-		"span_id":  	   spanContext.SpanID()})
+		"trace_id":        spanContext.TraceID(),
+		"span_id":         spanContext.SpanID()})
 
 	if v, ok := r.Context().Value(ctxKeySessionID{}).(string); ok {
 		log = log.WithField("session", v)
@@ -101,7 +102,9 @@ func (lh *logHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func readUserIP(r *http.Request) string {
 	ips := []string{"192.63.196.161", "172.217.18.110", "52.220.125.74", "151.101.194.28", "13.114.171.8", "34.206.39.153", "23.210.254.113",
-		"151.101.129.111", "142.250.185.67", "151.101.64.144", "158.46.145.28"}
+		"151.101.129.111", "142.250.185.67", "151.101.64.144", "158.46.145.28", "75.126.123.197", "71.211.166.198", "161.123.212.18", "68.198.186.48",
+		"68.198.186.40", "207.99.53.147", "91.214.87.74", "193.62.157.66", "5.157.21.190", "101.110.63.255", "52.219.8.20", "154.5.171.114",
+		"192.206.151.131", "46.14.53.1", "157.131.72.157", "147.237.70.107", "161.226.217.83", "23.81.93.62", "66.253.160.0", "220.41.101.217"}
 
 	return ips[rand.Intn(len(ips))]
 }
